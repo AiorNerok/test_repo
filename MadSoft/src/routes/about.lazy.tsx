0c2dcf1da6b5e60ac08680033a7f22ee2374c612
AiorNerok.Nujabes <90855034+AiorNerok@users.noreply.github.com>
2024-04-18 21:46:30 +0300

    upd

--- MadSoft/src/routes/about.lazy.tsx
+++ MadSoft/src/routes/about.lazy.tsx
@@ -5,5 +5,14 @@ export const Route = createLazyFileRoute("/about")({
 });
 
 function About() {
-  return <div className="p-2">Hello from About!</div>;
+  return <div className="p-2">
+    <h2>Стэк:</h2>
+    <p> "react": "^18.2.0"</p>
+    <p> "react-hook-form": "^7.51.3"</p>
+    <p> "react-timer-hook": "^3.0.7"</p>
+    <p> "sonner": "^1.4.41"</p>
+    <p> "uuid": "^9.0.1"</p>
+    <p> "zod": "^3.22.4"</p>
+    <p> "zustand": "^4.5.2</p>
+  </div>;
 }
